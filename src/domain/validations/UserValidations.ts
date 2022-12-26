76cd53301fc1947c926f72aa7ad12a50aff56244
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2022-12-26 19:03:54 -0300

    refactor: Separando o schema da função validate e validando a compatibilidade dos emails no próprio schema

--- src/domain/validations/UserValidations.ts
+++ src/domain/validations/UserValidations.ts
@@ -1,29 +1,12 @@
-import { object, string, number, date, boolean } from 'yup'
 import { Request, Response, NextFunction } from 'express'
+
+import { userSchema } from '../userSchema/userSchema'
 
 export class UserValidate {
 
     async validate(req: Request, res: Response, next: NextFunction) {
 
-        const schema = object().shape({
-
-            fullName: string().required(),
-            email: string().email().required(),
-            emailConfirmation: string().email().required(),
-            cpf: string().required(),
-            cellphone: string().required(),
-            birthdate: date().required(),
-            emailSms: boolean().required(),
-            whatsapp: boolean().required(),
-            country: string().required(),
-            city: string().required(),
-            postalCode: string().required(),
-            address: string().required(),
-            number: number().required()
-
-        })
-
-        if (!await(schema.isValid(req.body))) {
+        if (!await (userSchema.isValid(req.body))) {
 
             return res.status(404).json({
                 erro: true,
