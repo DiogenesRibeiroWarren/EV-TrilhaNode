7de7b558296c8f9726c0dd04e3a9b5e86e4c9324
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2022-12-26 19:06:21 -0300

    style: Seprando os métodos HTTP e exportando o router isoladamente

--- src/router.ts
+++ src/router.ts
@@ -7,8 +7,10 @@ import { createUserController } from './useCases/CreateUser/userIndex'
 
 const router = Router()
 
+router.get('/users', listUserUseCase.list)
+
+router.post('/customer', userValidateInstance.validate, (req: Request, res: Response) => {
+    return createUserController.handle(req, res)
+})
+
 export default router
-    .get('/users', listUserUseCase.list)
-    .post('/customer', userValidateInstance.validate, (req: Request, res: Response) => {
-        return  createUserController.handle(req, res)
-    })
