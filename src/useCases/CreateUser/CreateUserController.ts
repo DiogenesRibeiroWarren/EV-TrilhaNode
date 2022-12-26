bfc9c106d92e11ed9e184930c2192763a4e8a007
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2022-12-26 18:59:07 -0300

    style: Removendo linhas em branco

--- src/useCases/CreateUser/CreateUserController.ts
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -3,25 +3,20 @@ import { Request, Response } from 'express'
 import { CreateUserUseCase } from './CreateUserUseCase'
 import { ICreateUserDTO } from './CreateUserDTO'
 
-export class CreateUerController{
-
+export class CreateUerController {
     constructor(
-       private createUserUseCase: CreateUserUseCase 
-    ){}
+        private createUserUseCase: CreateUserUseCase
+    ) { }
 
-    handle(req: Request, res: Response){
+    handle(req: Request, res: Response) {
 
         const userData: ICreateUserDTO = req.body
 
-        try{
-
+        try {
             this.createUserUseCase.create(userData)
             return res.status(201).send('User created')
-
-        }catch(e: any){
+        } catch (e: any) {
             console.log(e)
         }
-
     }
-
 }
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -4,10 +4,6 @@ import { UserMock } from '../../domain/user/mocks/User'
 export class CreateUserUseCase {
 
     create(data: ICreateUserDTO) {
-
-        if (data.email != data.emailConfirmation)
-            throw new Error('The emails do not match')
-
         UserMock.push(data)
     }
 
