04202b0ac269b83158e3d14c5a9bb32dd92f37c0
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-01-02 16:46:02 -0300

    style: Quebra de linha nos parâmetros do construtor

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -15,10 +15,19 @@ export class User {
     public number: number;
 
     constructor(
-        fullName: string, email: string, emailConfirmation: string,
-        cpf: string, cellphone: string, birthdate: Date, emailSms: boolean,
-        whatsapp: boolean, country: string, city: string, postalCode: string,
-        address: string, number: number
+        fullName: string,
+        email: string,
+        emailConfirmation: string,
+        cpf: string,
+        cellphone: string,
+        birthdate: Date,
+        emailSms: boolean,
+        whatsapp: boolean,
+        country: string,
+        city: string,
+        postalCode: string,
+        address: string,
+        number: number
     ) {
         this.fullName = fullName,
             this.email = email,
