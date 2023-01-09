81b02d3c8b297d8c2ab52afa9d56497566029fa1
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-01-09 14:00:07 -0300

    feat: ALterando o valor do retorno da função

--- src/repositories/implementations/CreateUserRepository.ts
+++ src/repositories/implementations/CreateUserRepository.ts
@@ -3,7 +3,10 @@ import { User } from "../../entities/User";
 import { ICreateUserRepository } from "../ICreateUserRepository";
 
 export class CreateUserRepository implements ICreateUserRepository {
-    save(user: User): void {
+
+    save(user: User): User {
         UserMock.push(user)
+        return user
     }
+
 }
