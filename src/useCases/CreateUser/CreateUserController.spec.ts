794a36ca39f10bb2ff4c427d4078cb513f34b2b4
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-01-13 14:35:31 -0300

    refactor: Criando uma pasta para mockar os dados do usuário

--- src/useCases/CreateUser/CreateUserController.spec.ts
+++ src/useCases/CreateUser/CreateUserController.spec.ts
@@ -5,23 +5,10 @@ import { Request, Response } from 'express'
 import { CreateUerController } from './CreateUserController'
 import { CreateUserService } from './CreateUserService'
 import { CreateUserRepository } from '../../repositories/implementations/CreateUserRepository'
+import { userData } from '../../domain/userMocked/userMocked'
 
 const mockRequest = {
-    body: {
-        fullName: "Diógenes Ribeiro de Oliveira",
-        email: "diogenes.ribeiro@gmail.com",
-        emailConfirmation: "ok.ribeiro@gmail.com",
-        cpf: "872.027.195-65",
-        cellphone: "912276462",
-        birthdate: "2002-01-19",
-        emailSms: false,
-        whatsapp: true,
-        country: "Brasil",
-        city: "POA",
-        postalCode: "91450-310",
-        address: "Gildo de freitas",
-        number: 20
-    },
+    body: userData
 } as Request;
 
 const mockResponse = {
--- src/useCases/CreateUser/CreateUserService.spec.ts
+++ src/useCases/CreateUser/CreateUserService.spec.ts
@@ -2,24 +2,8 @@ import 'reflect-metadata'
 
 import { User } from "../../entities/User";
 import { CreateUserRepository } from "../../repositories/implementations/CreateUserRepository";
-import { ICreateUserDTO } from './CreateUserDTO';
+import { userData } from '../../domain/userMocked/userMocked';
 import { CreateUserService } from "./CreateUserService";
-
-const userData: ICreateUserDTO = {
-    fullName: "Diógenes Ribeiro de Oliveira",
-    email: "leandro.ribeiro@gmail.com",
-    emailConfirmation: "leandro.ribeiro@gmail.com",
-    cpf: "872.027.195-65",
-    cellphone: "912276462",
-    birthdate: "2002-01-19",
-    emailSms: false,
-    whatsapp: true,
-    country: "Brasil",
-    city: "POA",
-    postalCode: "91450-310",
-    address: "Gildo de freitas",
-    number: 20
-}
 
 const createUserRepository = new CreateUserRepository()
 const createUserService = new CreateUserService(createUserRepository)
