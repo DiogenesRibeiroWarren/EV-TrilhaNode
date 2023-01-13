1806d73c93045e36aa9f460aa2c7138864471d79
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-01-13 15:05:33 -0300

    fix: adicionando o @injectable()

--- src/useCases/CreateUser/CreateUserService.ts
+++ src/useCases/CreateUser/CreateUserService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from "tsyringe";
 import { User } from "../../entities/User";
 import { ICreateUserRepository } from "../../repositories/ICreateUserRepository";
 
+@injectable()
 export class CreateUserService {
 
     constructor(
