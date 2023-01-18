6fd972852289924fff448023f61bc1307db41b8e
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-01-18 13:46:42 -0300

    refactor: Separando o app.listener do arquivo main e exportando a constant app

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
-import 'reflect-metadata'
-import './shared/container'
+import { app } from "./main";
 
-import express from 'express';
-import router from './router';
-
-const app = express();
 const PORT = 8080;
-
-app.use(express.json());
-app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
