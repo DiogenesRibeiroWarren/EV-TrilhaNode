cdf103604d46d20b68878df6b8ac0caa2fb9d875
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-01-30 16:19:01 -0300

    feat: Salvando o usuário no banco de dados

--- src/repositories/implementations/CreateUserRepository.ts
+++ src/repositories/implementations/CreateUserRepository.ts
@@ -2,10 +2,14 @@ import { UserMock } from "../../domain/user/mocks/User";
 import { User } from "../../entities/User";
 import { ICreateUserRepository } from "../ICreateUserRepository";
 
+import { dataBaseClienteInstance } from "../../infra/mongoDB/indexMongoDBClient";
+
 export class CreateUserRepository implements ICreateUserRepository {
 
-    save(user: User): User {
-        UserMock.push(user)
+    async save(user: User): Promise<User> {
+        const collection = dataBaseClienteInstance.getInstance().collection('customer')
+        await collection.insertMany([user])
+
         return user
     }
 
