e7eb6894ce006fd69231f08943b0239139e6c1d4
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-01-30 16:55:41 -0300

    feat: Conectando no banco de dados ao inicializar o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import { dataBaseClienteInstance } from "./infra/mongoDB/indexMongoDBClient";
 import { app } from "./main";
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+    dataBaseClienteInstance.connect()
+        .then(console.log)
+        .catch(console.error)
+
+    console.log(`Server running on port: ${PORT}`)
 });
