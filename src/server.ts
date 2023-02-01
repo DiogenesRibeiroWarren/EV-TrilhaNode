7760c42cd392f0937bf8e29af0da1cfaacafd294
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-02-01 14:34:14 -0300

    feat: Inserindo a porta do servidor no arquivo .dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import { dataBaseClienteInstance } from "./infra/mongoDB/indexMongoDBClient";
 import { app } from "./main";
 
-const PORT = 8080;
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: __dirname + '/.env'
+})
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     dataBaseClienteInstance.connect()
         .then(console.log)
-        .catch(console.error)
+        .catch(() => dataBaseClienteInstance.close())
 
     console.log(`Server running on port: ${PORT}`)
+
 });
