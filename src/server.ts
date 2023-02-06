49d1f4285d60d08aad8f90c050927aacaab14a9e
Diógenes Ribeiro <diogenes.ribeiro@warren.com.br>
2023-02-06 13:52:37 -0300

    fix: Inicializando a conexão com o banco de daos antes de inicializar o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
 import { dataBaseClienteInstance } from "./infra/mongoDB/indexMongoDBClient";
 import { app } from "./main";
 
-import dotenv from 'dotenv'
-
-dotenv.config({
-    path: __dirname + '/.env'
-})
-
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    dataBaseClienteInstance.connect()
+app.listen(PORT, async () => {
+    await dataBaseClienteInstance.connect()
         .then(console.log)
         .catch(() => dataBaseClienteInstance.close())
 
     console.log(`Server running on port: ${PORT}`)
-
 });
